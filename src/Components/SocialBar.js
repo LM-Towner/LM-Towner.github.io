e409dad10427f3e835dc61c2dc9e0c9bb681f2dd
La-Keisha Towner <ltowner622@gmail.com>
2017-11-10 17:16:10 -0500

    add classname

--- src/Components/SocialBar.js
+++ src/Components/SocialBar.js
@@ -3,7 +3,7 @@ import '../styles/SocialBar.css';
 
 export default class SocialBar extends Component {
   render() {
-    return <div id='social-container'>
+    return <div id='social-container' className='col-md-6'>
         <a className="social-icon" href="https://facebook.com">
           <i className="devicon-facebook-plain colored" />
         </a>
