50f33ec10abce228bbac59a472db5c2bcb10d5c1
La-Keisha Towner <ltowner622@gmail.com>
2017-11-10 15:24:35 -0500

    Imported Main SideBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/App.css";
+import MainSideBar from './Components/MainSideBar';
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className='col-md-12'>
+        <MainSideBar/>
+      </div>
     );
   }
 }
